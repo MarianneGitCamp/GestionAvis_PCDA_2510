96c15aa33e132c4d432569878042127720c5c4d0
Quentin-ENI <qprigent@campus-eni.fr>
2026-01-16 14:32:24 +0100

    First commit

---
+++ src/test/java/com/eni/gestionavis/GestionAvisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eni.gestionavis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAvisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
